c563e52507965018a5c30ed9ac4ad04f0606ef40
D <dylanduretz@gmail.com>
2023-11-14 06:28:31 +0000

    Login Server And launch type selection working

--- src-tauri/src/commands/launch_game_command.rs
+++ src-tauri/src/commands/launch_game_command.rs
@@ -3,10 +3,16 @@
 use tauri::Result;
 use std::process::Command as SystemCommand;
 
+#[derive(serde::Deserialize)]
+pub struct LaunchOptions {
+    exePath: Option<String>,
+    launchArg: String,
+}
+
 #[tauri::command]
-pub fn launch_game() -> Result<()> {
-    let app_id = "17080"; // App ID for Tribes:Ascend on steam
-    let steam_url = format!("steam://rungameid/{}", app_id);
+pub fn launch_game(options: LaunchOptions) -> Result<()> {
+    let app_id = "17080"; // App ID for Tribes: Ascend on Steam
+    let steam_url = format!("steam://rungameid/{}//{}", app_id, options.launchArg);
 
     SystemCommand::new("cmd")
         .args(["/C", "start", "", &steam_url])
@@ -14,14 +20,15 @@ pub fn launch_game() -> Result<()> {
         .map(|_| ())
         .map_err(|e| e.into())
 }
-// TODO fix launch args
 
 #[tauri::command]
-pub fn launch_game_non_steam() -> Result<()> {
-    SystemCommand::new("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Tribes\\Binaries\\Win32\\TribesAscend.exe")
-        .args(["-hostx=ta.kfk4ever.com"])
+pub fn launch_game_non_steam(options: LaunchOptions) -> Result<()> {
+    // Use the provided executable path or a default one
+    let exe_path = options.exePath.unwrap_or_else(|| "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Tribes\\Binaries\\Win32\\TribesAscend.exe".to_string());
+
+    SystemCommand::new(&exe_path)
+        .args(["-hostx=Ta.dodgesdomain.com", &options.launchArg])
         .spawn()
         .map(|_| ()) // Convert Ok(Child) to Ok(())
         .map_err(|e| e.into()) // Convert std::io::Error to tauri::Error
 }
-// .args(["-hostx=Ta.dodgesdomain.com"])
--- src/components/GameLauncher/GameLauncher.tsx
+++ src/components/GameLauncher/GameLauncher.tsx
@@ -1,35 +1,75 @@
 // GameLauncher.tsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
-import { Card, Image, Button, Text, useMantineTheme  } from '@mantine/core';
-import { FaCirclePlay } from'react-icons/fa6';
+import { Card, Image, Button, Text, useMantineTheme, Group, TextInput, ActionIcon } from '@mantine/core';
+import { FaCirclePlay, FaFolderOpen } from 'react-icons/fa6';
+import { FaSearch } from 'react-icons/fa';
 import classes from './GameLauncher.module.css';
-
 
 const GameLauncher: React.FC = () => {
   const theme = useMantineTheme();
+  const [launchType, setLaunchType] = useState('Steam');
+  const [loginServer, setLoginServer] = useState('Community');
+  const [exePath, setExePath] = useState('');
+  const [customServer, setCustomServer] = useState('');
 
   const startGame = async () => {
     try {
-      await invoke("launch_game");
-      console.log("Tribes Ascend launched successfully");
+      // Determine the appropriate launch argument based on the selected login server
+      let launchArg;
+      switch (loginServer) {
+        case 'Community':
+          launchArg = '-hostx=ta.kfk4ever.com';
+          break;
+        case 'PUG':
+          launchArg = '-hostx=Ta.dodgesdomain.com';
+          break;
+        case 'Custom':
+          // Temporary argument for Custom, can be updated later
+          launchArg = '-hostx=Ta.dodgesdomain.com';
+          break;
+        default:
+          launchArg = '';
+      }
+  
+      let options = {
+        exePath,
+        launchArg,
+      };
+  
+      if (launchType === 'Steam') {
+        // Call the Rust function for Steam
+        await invoke("launch_game", { options });
+        console.log("Tribes Ascend launched successfully with Steam options:", options);
+      } else if (launchType === 'Non Steam') {
+        // Call the Rust function for Non-Steam
+        await invoke("launch_game_non_steam", { options });
+        console.log("Tribes Ascend launched successfully with Non-Steam options:", options);
+      }
     } catch (error) {
       console.error("Failed to launch Tribes Ascend", error);
     }
   };
+  
+
+  const buttonStyle = (selected: boolean) => ({
+    backgroundColor: selected ? theme.colors.mutedBlue[9] : 'rgba(0, 0, 0, 0.1)',
+    color: selected ? theme.colors.darkGray[3] : theme.colors.lightGray[3],
+    fontWeight: selected ? 'bold' : 'normal',
+  });
 
   return (
     <Card 
       shadow="lg" 
       padding="lg" 
       radius="sm" 
-      style={{ backgroundColor: '#2C3E50', maxWidth: '350px', margin: 'auto' }} // Adjust size here
+      style={{ backgroundColor: '#2C3E50', maxWidth: '350px', margin: 'auto' }}
     >
       <Card.Section>
         <Image
           src="https://i.ibb.co/4dxGMxq/ds.jpg"
-          height={160} // Adjusted height
+          height={160}
           alt="Tribes Ascend"
         />
       </Card.Section>
@@ -38,9 +78,92 @@ const GameLauncher: React.FC = () => {
         Tribes: Ascend Launcher
       </Text>
 
-      <Text size="sm" style={{ marginTop: '0.5rem', color: '#CCCCCC' }}>
-        
-      </Text>
+      <Text 
+      size="sm" 
+      style={{ 
+        marginTop: '1rem', 
+        color: theme.colors.lightGray[7],
+        fontWeight: 'bold',
+       }}
+      >Launch Type:</Text>
+      <Button.Group>
+        <Button 
+          onClick={() => setLaunchType('Steam')} 
+          style={buttonStyle(launchType === 'Steam')}
+        >
+          Steam
+        </Button>
+        <Button 
+          onClick={() => setLaunchType('Non Steam')} 
+          style={buttonStyle(launchType === 'Non Steam')}
+        >
+          Non Steam
+        </Button>
+      </Button.Group>
+
+      {launchType === 'Non Steam' && (
+        <Group grow>
+          <ActionIcon>
+            <FaFolderOpen />
+          </ActionIcon>
+          <TextInput
+            value={exePath}
+            onChange={(event) => setExePath(event.currentTarget.value)}
+            description="Tribes Ascend executable path"
+            placeholder="Path to executable"
+            error="Invalid Path"
+            styles={{
+              input: {
+                color: 'black',
+                backgroundColor: 'lightgray',
+                width: '100%'
+              },
+            }}
+          />
+          <ActionIcon>
+            <FaSearch />
+          </ActionIcon>
+        </Group>
+      )}
+
+    <Text 
+      size="sm" 
+      style={{ 
+        marginTop: '1rem', 
+        color: theme.colors.lightGray[7],
+        fontWeight: 'bold',
+       }}
+      >
+        Login Server:</Text>
+      <Button.Group>
+        <Button 
+          onClick={() => setLoginServer('Community')} 
+          style={buttonStyle(loginServer === 'Community')}
+        >
+          Community
+        </Button>
+        <Button 
+          onClick={() => setLoginServer('PUG')} 
+          style={buttonStyle(loginServer === 'PUG')}
+        >
+          PUG
+        </Button>
+        <Button 
+          onClick={() => setLoginServer('Custom')} 
+          style={buttonStyle(loginServer === 'Custom')}
+        >
+          Custom
+        </Button>
+      </Button.Group>
+
+      {loginServer === 'Custom' && (
+        <TextInput
+          label="Custom Login Server"
+          placeholder="Enter custom server"
+          value={customServer}
+          onChange={(event) => setCustomServer(event.currentTarget.value)}
+        />
+      )}
 
       <Button 
         onClick={startGame} 
