184bb8dcf09d06c56e6565384199de7cb4048afa
D <dylanduretz@gmail.com>
2023-11-15 06:18:52 +0000

    Refactored downloader

--- src-tauri/src/commands/downloader_command.rs
+++ src-tauri/src/commands/downloader_command.rs
@@ -1,26 +1,21 @@
 // downloader_command.rs
 
 use std::path::PathBuf;
-use std::sync::atomic::{AtomicBool, Ordering};
 use futures::StreamExt;
 use serde::{Deserialize, Serialize};
 use tauri::{AppHandle, Manager};
-use tauri::api::path::download_dir;
 use std::env;
 use tokio::io::AsyncWriteExt;
 
-// Global or shared state to control the download
-static DOWNLOAD_CONTROL: AtomicBool = AtomicBool::new(true);
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Progress {
-    pub download_id: i64,
+    pub package_id: String,
     pub filesize: u64,
     pub transfered: u64,
     pub transfer_rate: f64,
     pub percentage: f64,
 }
-
 
 
 impl Progress {
@@ -33,6 +28,22 @@ impl Progress {
     }
 }
 
+// New function to determine download path based on package type
+fn determine_download_path(package_id: &str) -> Result<PathBuf, String> {
+    let username = env::var("USERNAME").unwrap_or_else(|_| "default".into());
+    let tribes_path = PathBuf::from(format!(r"C:\Users\{}\Documents\My Games\Tribes Ascend\TribesGame", username));
+    let ta_launcher_path = tribes_path.join("TALauncher");
+
+    // Check package type and return the corresponding path
+    match package_id {
+        id if id.contains("dll") => Ok(ta_launcher_path.clone()),
+        id if id.contains("game_files") => Ok(ta_launcher_path.join("Game Files")), // Adjusted path for game_files
+        id if id.contains("config") => Ok(tribes_path.join("Tribes Config")),
+        _ => Err("Unrecognized package type".to_string()),
+    }
+}
+
+
 #[tauri::command]
 pub async fn download_package(
     package_id: String, 
@@ -40,7 +51,7 @@ pub async fn download_package(
     handle: tauri::AppHandle // Pass the app handle to emit events
 ) -> Result<String, String> {
     let base_url = "https://tamods-update.s3.ap-southeast-2.amazonaws.com/";
-    
+
     // Check if the package_id contains 'dll'
     let object_key = if package_id.contains("dll") {
         format!("packages/{}.zip", package_id)
@@ -51,29 +62,14 @@ pub async fn download_package(
     let download_url = format!("{}{}", base_url, object_key);
     println!("Download url: {}", download_url);
 
-    // Determine the download path
-    let download_path = if object_key.contains("dll") {
-        let username = env::var("USERNAME").unwrap_or_else(|_| "default".into());
-        let tribes_path = PathBuf::from(format!(r"C:\Users\{}\Documents\My Games\Tribes Ascend\TribesGame\TALauncher", username));
-        if !tribes_path.exists() {
-            tokio::fs::create_dir_all(&tribes_path).await.map_err(|e| e.to_string())?;
-        }
-        tribes_path
-    } else {
-        if cfg!(target_os = "windows") {
-            PathBuf::from(env::var("USERPROFILE").unwrap_or_else(|_| "C:\\".into()))
-                .join("Downloads")
-        } else {
-            download_dir().unwrap_or_else(|| PathBuf::from("downloads"))
-        }
-    };
+    let download_path = determine_download_path(&package_id)?;
 
     let file_path = download_path.join(object_key.split('/').last().unwrap());
     println!("File Path: {:?}", file_path);
 
     // Initialize progress tracking
     let mut progress = Progress {
-        download_id: 0, // Set an appropriate download ID
+        package_id: package_id.clone(),
         filesize,
         transfered: 0,
         transfer_rate: 0.0,
@@ -94,11 +90,6 @@ pub async fn download_package(
 
     // Stream and write the download
     while let Some(item) = stream.next().await {
-        // Check if the download should continue
-        if !DOWNLOAD_CONTROL.load(Ordering::SeqCst) {
-            break; // or wait until DOWNLOAD_CONTROL is true again
-        }
-        
         let chunk = item.map_err(|e| e.to_string())?;
         progress.transfered += chunk.len() as u64;
         temp_zip_file.write_all(&chunk).await.map_err(|e| e.to_string())?;
@@ -144,5 +135,4 @@ pub async fn download_package(
 
     Ok(format!("Extracted to {:?}", download_path))
 }
-
 
