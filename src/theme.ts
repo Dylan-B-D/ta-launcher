a18c8369c2a7c5a82a958c949cf633ef5f815748
D <dylanduretz@gmail.com>
2023-11-16 14:16:38 +0000

    Created simple extensible theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,103 +1,136 @@
 // theme.ts
-import { createTheme } from "@mantine/core";
+import { MantineTheme, createTheme } from "@mantine/core";
+import { useThemeContext } from "./context/ThemeContext";
 
-export const darkTheme = createTheme({
-  // Color palette
-  colors: {
-    mutedBlue: [
-      "#2C3E50",
-      "#34495E",
-      "#3B546B",
-      "#425F79",
-      "#4A6A86",
-      "#527594",
-      "#5A80A1",
-      "#628BAF",
-      "#6A96BC",
-      "#729FCA",
-    ],
+declare module "@mantine/core" {
+  interface MantineTheme {
+    secondaryColor: string;
+    highlightColor: string;
+  }
+}
 
-    mutedAmber: [
-      "#50392C", // Dark, rich amber
-      "#6C4F3D", // Dark muted amber
-      "#876452", // Deep muted amber
-      "#A27A68", // Rich muted amber
-      "#BD8F7D", // Mid-tone muted amber
-      "#D8A593", // Neutral muted amber
-      "#F3BCA8", // Light muted amber
-      "#FFD2BE", // Very light muted amber
-      "#FFE8D4", // Pale muted amber
-      "#FFF3E9", // Almost white amber
-    ],
+const createAppTheme = () => {
+  const { primaryColor } = useThemeContext();
+  return createTheme({
+    fontFamily: "Nunito Sans",
 
+    // https://mantine.dev/colors-generator/
+    // Colors for UI components, try to keep intensity range normalised.
+    // Intensity ranges should be from light to dark.
 
-    // Additional muted colors
-    mutedRed: [
-      "#542424",
-      "#633030",
-      "#723C3C",
-      "#814848",
-      "#905454",
-      "#9F6060",
-      "#AE6C6C",
-      "#BD7878",
-      "#CC8484",
-      "#DB9090",
-    ],
-
-    mutedPurple: [
-      "#423C52",
-      "#504862",
-      "#5E5472",
-      "#6C6082",
-      "#7A6C92",
-      "#8878A2",
-      "#9684B2",
-      "#A490C2",
-      "#B29CD2",
-      "#C0A8E2",
-    ],
-
-
-    lightGray: [
-      "#f8f9fa", // Very light gray
-      "#f2f3f4", // Slightly darker
-      "#eceef0", // Light gray
-      "#e6e8eb", // Mid-light gray
-      "#dfe1e3", // Neutral gray
-      "#d9dbdd", // Slightly darker gray
-      "#d3d5d7", // Darker gray
-      "#cdced0", // Medium gray
-      "#c7c8c9", // Medium-dark gray
-      "#c1c2c3", // Darkest shade in light gray range
-    ],
-
-    darkGray: [
-      "#0a0b0d", // Almost black
-      "#131415", // Very dark gray
-      "#1c1d1f", // Darker gray
-      "#252729", // Dark gray
-      "#2e3032", // Mid-dark gray
-      "#37393b", // Neutral dark gray
-      "#404346", // Slightly lighter gray
-      "#494c4f", // Medium-dark gray
-      "#525559", // Medium gray
-      "#5b5e61", // Lightest shade in dark gray range
-    ],
-
-  },
-
-  // Customize other theme properties as desired
-  primaryColor: "mutedBlue", // Using the muted blue as the primary color
-  shadows: {
-    md: "0px 2px 4px rgba(0, 0, 0, 0.5)", // Custom shadow
-    xl: "0px 4px 8px rgba(0, 0, 0, 0.6)", // Custom shadow
-  },
-  headings: {
-    fontFamily: "Arial, sans-serif", // Custom font for headings
-    sizes: {
-      h1: { fontSize: "24px", fontWeight: "500" },
-      h2: { fontSize: "20px", fontWeight: "500" },
+    colors: {
+      mutedBlue: [
+        "#2C3E50",
+        "#34495E",
+        "#3B546B",
+        "#425F79",
+        "#4A6A86",
+        "#527594",
+        "#5A80A1",
+        "#628BAF",
+        "#6A96BC",
+        "#729FCA",
+      ],
+      mutedAmber: [
+        "#50392C",
+        "#6C4F3D",
+        "#876452",
+        "#A27A68",
+        "#BD8F7D",
+        "#D8A593",
+        "#F3BCA8",
+        "#FFD2BE",
+        "#FFE8D4",
+        "#FFF3E9",
+      ],
+      mutedRed: [
+        "#542424",
+        "#633030",
+        "#723C3C",
+        "#814848",
+        "#905454",
+        "#9F6060",
+        "#AE6C6C",
+        "#BD7878",
+        "#CC8484",
+        "#DB9090",
+      ],
+      mutedGreen: [
+        "#2C503E",
+        "#345E4A",
+        "#3B6B56",
+        "#427962",
+        "#4A866E",
+        "#52947A",
+        "#5AA186",
+        "#62AF92",
+        "#6ABC9E",
+        "#72C9AA",
+      ],
+      mutedPurple: [
+        "#352C50",
+        "#40345E",
+        "#4B3B6B",
+        "#564279",
+        "#614A86",
+        "#6C5294",
+        "#775AA1",
+        "#8262AF",
+        "#8D6ABC",
+        "#9872C9",
+      ],
+      mutedOrange: [
+        "#503C2C",
+        "#5E4634",
+        "#6B5041",
+        "#795A4E",
+        "#86645A",
+        "#94706C",
+        "#A17A78",
+        "#AF8485",
+        "#BC8E92",
+        "#C9989F",
+      ],
+      mutedTeal: [
+        "#2C504C",
+        "#345E59",
+        "#3B6B66",
+        "#427973",
+        "#4A8680",
+        "#52948D",
+        "#5AA19A",
+        "#62AFA7",
+        "#6ABCB4",
+        "#72C9C1",
+      ],
+      mutedPink: [
+        "#502C48",
+        "#5E3457",
+        "#6B3B65",
+        "#794273",
+        "#864A81",
+        "#94528F",
+        "#A15A9D",
+        "#AF62AA",
+        "#BC6AB8",
+        "#C972C5",
+      ],
     },
-  },
-});
+    primaryColor,
+    secondaryColor: "mutedAmber",
+    highlightColor: "mutedRed",
+    components: {
+      Button: {
+        styles: (theme: MantineTheme) => ({
+          root: {
+            color: theme.colors[theme.secondaryColor][5],
+            backgroundColor: theme.colors[theme.primaryColor][5],
+            borderColor: theme.colors[theme.highlightColor][5],
+          },
+        }),
+      },
+    },
+  });
+};
+
+export default createAppTheme;
