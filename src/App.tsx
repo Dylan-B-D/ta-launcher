3951c024709e32a050b1842ad59d420a5b63afed
D <dylanduretz@gmail.com>
2023-11-17 17:13:32 +0000

    Added mantine notifications

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './Routes/AppRoutes';
 import { views } from './Routes/routes';
+import { Notifications } from '@mantine/notifications';
 
 // UI components and hooks
 import { AppShell, MantineProvider } from '@mantine/core';
@@ -14,10 +15,11 @@ import createAppTheme from './theme';
 import { NavbarNested } from './components/NavbarNested/NavbarNested';
 import { appWindow, PhysicalSize } from '@tauri-apps/api/window';
 import HeaderComponent from './components/Header/Header';
+import classes from './styles.module.css';
 
 
 async function setMinWindowSize() {
-  await appWindow.setMinSize(new PhysicalSize(600, 500));
+  await appWindow.setMinSize(new PhysicalSize(580, 380));
 }
 
 
@@ -28,6 +30,7 @@ function App() {
   return (
     <Router>
       <MantineProvider theme={theme} defaultColorScheme="dark">
+        <Notifications className={classes.mantineNotificationsContainer}/>
         <AppShell header={{ height: 30 }} navbar={{ width: 220, breakpoint: 'none' }} padding="md">
           <HeaderComponent />
           <AppShell.Navbar>
