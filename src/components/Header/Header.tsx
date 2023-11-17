a19f41aacc008d614da1a943b5ea27c3bf4fef6d
D <dylanduretz@gmail.com>
2023-11-17 21:01:52 +0000

    Fix header alignment

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 // HeaderComponent.tsx
 import React, { useEffect, useState } from 'react';
-import { AppShell, Badge, Progress, useMantineTheme } from '@mantine/core';
+import { AppShell, Badge, Code, Progress, useMantineTheme } from '@mantine/core';
 import { invoke } from '@tauri-apps/api';
 import { listen } from '@tauri-apps/api/event';
 import { formatSpeed } from '../../utils.ts';
@@ -47,13 +47,13 @@ const HeaderComponent: React.FC = () => {
       setDownloadProgress(null); // Reset progress on completion
     };
 
-    // Listening to download progress
+
     const unsubscribeProgress = listen('DOWNLOAD_PROGRESS', handleDownloadProgress);
 
-    // Listening to download completion
+
     const unsubscribeComplete = listen('DOWNLOAD_FINISHED', handleDownloadComplete);
 
-    // Clean up the listeners when the component is unmounted
+    
     return () => {
       unsubscribeProgress.then((fn) => fn());
       unsubscribeComplete.then((fn) => fn());
@@ -66,45 +66,47 @@ const HeaderComponent: React.FC = () => {
         header: {
           borderBottomWidth: '1px',
           borderBottomStyle: 'solid',
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'space-between',
         },
       })}
     >
-      <div className="flex justify-between items-center">
-        <div className="flex-1"></div> {/* Invisible spacer */}
-
-        {/* Centered download progress and text */}
-        {downloadProgress && (
-          <div className="flex items-center justify-center flex-grow" style={{ width: '100%', maxWidth: '400px' }}>
-            <div style={{ width: '200px' }}> {/* Fixed width for the progress bar */}
-              <Progress
-                value={downloadProgress.percentage}
-                color="green"
-                animated
-                size="xs"
-                style={{ backgroundColor: 'black' }}
-              />
-            </div>
-            <div style={{ width: '150px', marginLeft: '15px' }}> {/* Fixed width for the text */}
-              <span style={{ whiteSpace: 'nowrap' }}>
-                {downloadProgress.percentage.toFixed(2)}% at {formatSpeed(downloadProgress.transfer_rate)}
-              </span>
-            </div>
-          </div>
-        )}
-
-
-        <div className="flex items-center pr-4">
-          <Badge color="mutedBlue" variant="filled" style={{
-            marginLeft: '10px', marginTop: '5px', color: theme.colors?.darkGray?.[3] || '#B0B0B0'
-          }}>
-            Community: {playerCounts.Community}
-          </Badge>
-          <Badge color="mutedBlue" variant="filled" style={{
-            marginLeft: '10px', marginTop: '5px', color: theme.colors?.darkGray?.[3] || '#B0B0B0'
-          }}>
-            PUG: {playerCounts.PUG}
-          </Badge>
+      {/* Left-aligned badges */}
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Badge color="mutedBlue" variant="filled" style={{
+          marginLeft: '10px', color: theme.colors?.darkGray?.[3] || '#B0B0B0'
+        }}>
+          Community: {playerCounts.Community}
+        </Badge>
+        <Badge color="mutedBlue" variant="filled" style={{
+          marginLeft: '10px', color: theme.colors?.darkGray?.[3] || '#B0B0B0'
+        }}>
+          PUG: {playerCounts.PUG}
+        </Badge>
+      </div>
+  
+      {/* Centered download progress and text */}
+      {downloadProgress && (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Progress
+            value={downloadProgress.percentage}
+            color="green"
+            animated
+            size="xs"
+            style={{ width: '200px', backgroundColor: 'black' }} // Fixed width for the progress bar
+          />
+          <span style={{ marginLeft: '15px', whiteSpace: 'nowrap', width: '150px' }}>
+            {downloadProgress.percentage.toFixed(2)}% at {formatSpeed(downloadProgress.transfer_rate)}
+          </span>
         </div>
+      )}
+  
+      {/* Right-aligned version code */}
+      <div style={{ paddingRight: '10px' }}>
+        <Code fw={700} style={{
+          color: theme.colors[theme.primaryColor][4],
+        }}>Alpha v0.0.1</Code>
       </div>
     </AppShell.Header>
   );
