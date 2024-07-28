74be0d2f33bcb86a6bd944c3d9a215af94a8aa5f
Dylan <dylanduretz@gmail.com>
2024-07-28 21:45:17 +0100

    Add file existance checks to avoid first time setup errors

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-import { writeTextFile, BaseDirectory, readTextFile } from '@tauri-apps/plugin-fs';;
+import { writeTextFile, BaseDirectory, readTextFile, exists } from '@tauri-apps/plugin-fs';;
 
 const DOWNLOADED_PACKAGES_FILE = 'downloaded_packages.json';
 const CONFIG_FILE = 'config.json';
@@ -9,7 +9,14 @@ export async function saveConfig(config: Record<string, any>) {
 
 export const loadConfig = async (setConfig: React.Dispatch<React.SetStateAction<any>>) => {
   try {
-    const savedConfig = await readTextFile('config.json', { baseDir: BaseDirectory.AppLocalData });
+    const fileExists = await exists(CONFIG_FILE, { baseDir: BaseDirectory.AppLocalData });
+
+    if (!fileExists) {
+      console.log('First-time setup: No config file found.');
+      return;
+    }
+
+    const savedConfig = await readTextFile(CONFIG_FILE, { baseDir: BaseDirectory.AppLocalData });
     if (savedConfig) {
       setConfig((prevConfig: Record<string, any>) => ({
         ...prevConfig,
@@ -17,14 +24,9 @@ export const loadConfig = async (setConfig: React.Dispatch<React.SetStateAction<
       }));
     }
   } catch (error) {
-    if ((error as any).code === 'ENOENT') {
-      console.log('First-time setup: No config file found.');
-    } else {
-      console.error('Failed to load config:', error);
-    }
+    console.error('Failed to load config:', error);
   }
 };
-
 export async function saveDownloadedPackages(packages: Map<string, string>) {
   await writeTextFile(
     DOWNLOADED_PACKAGES_FILE,
@@ -35,15 +37,17 @@ export async function saveDownloadedPackages(packages: Map<string, string>) {
 
 export async function loadDownloadedPackages(): Promise<Map<string, string>> {
   try {
+    const fileExists = await exists(DOWNLOADED_PACKAGES_FILE, { baseDir: BaseDirectory.AppLocalData });
+
+    if (!fileExists) {
+      console.log('Downloaded packages file not found, this is expected on the first launch.');
+      return new Map();
+    }
+
     const savedPackages = await readTextFile(DOWNLOADED_PACKAGES_FILE, { baseDir: BaseDirectory.AppLocalData });
     return new Map(Object.entries(JSON.parse(savedPackages)));
   } catch (error) {
-    if ((error as any).code === 'ENOENT') {
-      console.log('No downloaded packages file found.');
-      return new Map();
-    } else {
-      console.error('Failed to load downloaded packages:', error);
-      return new Map();
-    }
+    console.error('Failed to load downloaded packages:', error);
+    return new Map();
   }
 }
